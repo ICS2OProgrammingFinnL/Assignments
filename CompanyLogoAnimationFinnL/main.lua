adae2760c28180afe87ee286f6005aac3b153404
FinnLeduc <43419123+FinnLeduc@users.noreply.github.com>
2018-11-12 13:14:27 -0500

    fd

--- CompanyLogoAnimationFinnL/main.lua
+++ CompanyLogoAnimationFinnL/main.lua
@@ -1,7 +1,8 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--Name:
+-- Title: Company Logo Animation
+-- course:ICS20
+-- this will display the company lgog before playing the game 
 -----------------------------------------------------------------------------------------
 
 -- Your code here
